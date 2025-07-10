2d730a057cef8c67b97656e382990ea4fe5975e7
Juliocbs1 <barriossanchezjuliocesar86@gmail.com>
2025-07-10 11:57:59 -0500

    Se añade en el menu de opciones a cada uno de los items, las funciones para activar intents hacia las activity correspondientes

--- app/src/main/java/com/example/miscontactos/MainActivity.java
+++ app/src/main/java/com/example/miscontactos/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.miscontactos;
 
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.widget.Toolbar;
@@ -84,5 +88,22 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_opciones,menu);
         return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.mAbout) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+
+        }else if(itemId == R.id.mSettings){
+                Intent intent2 = new Intent(this, SettingsActivity.class);
+                startActivity(intent2);
+        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 }
